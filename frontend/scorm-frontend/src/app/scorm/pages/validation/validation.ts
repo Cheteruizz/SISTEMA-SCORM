a8dfdc3b4e01783914713c711f768d0ae04d8f42
Alfonso Ruiz Bataller <alfonsoruiz.24@campuscamara.es>
2025-11-19 22:55:33 +0100

    feat: avance en rutas SCORM y estructura de páginas

---
+++ frontend/scorm-frontend/src/app/scorm/pages/validation/validation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validation',
+  imports: [],
+  templateUrl: './validation.html',
+  styleUrl: './validation.scss',
+})
+export class Validation {
+
+}
