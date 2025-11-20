ddc2926aaeb5aae606322ca221e33bcd0410a8ad
alfonsoruiz.24@campuscamara.es <alfonsoruiz.24@campuscamara.es>
2025-11-20 12:45:10 +0100

    feat: añadir layout component y actualizar rutas SCORM

--- frontend/scorm-frontend/src/app/scorm/scorm.routes.ts
+++ frontend/scorm-frontend/src/app/scorm/scorm.routes.ts
@@ -1,9 +1,41 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout';
 
-export const ScormRoutes: Routes = [
+export const SCORM_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./pages/home/home.component').then(m => m.HomeComponent)
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home.component').then(m => m.HomeComponent),
+      },
+      {
+        path: 'metadata',
+        loadComponent: () =>
+          import('./pages/metadata/metadata').then(m => m.MetadataComponent),
+      },
+      {
+        path: 'organizations',
+        loadComponent: () =>
+          import('./pages/organizations/organizations').then(m => m.OrganizationsComponent),
+      },
+      {
+        path: 'resources',
+        loadComponent: () =>
+          import('./pages/resources/resources').then(m => m.ResourcesComponent),
+      },
+      {
+        path: 'preview',
+        loadComponent: () =>
+          import('./pages/preview/preview').then(m => m.PreviewComponent),
+      },
+      {
+        path: 'validation',
+        loadComponent: () =>
+          import('./pages/validation/validation').then(m => m.ValidationComponent),
+      },
+    ]
   }
 ];
