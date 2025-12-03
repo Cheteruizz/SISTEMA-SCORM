7bda390ef13d1ae0fcd2f247aba8e0989f8422b6
Juan Antonio.Brioso <juanantoniobrioso.24@campuscamara.es>
2025-12-03 14:33:08 +0100

    ini

--- frontend/scorm-frontend/src/app/scorm/pages/home/home.component.ts
+++ frontend/scorm-frontend/src/app/scorm/pages/home/home.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-scorm-home',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+ // Estado inicial: mostramos el menú principal
+  vistaActual: string = 'menu';
+
+  // Función para navegar
+  // Recibe el nombre de la vista a la que queremos ir ('menu', 'login', 'registro')
+  cambiarVista(vista: string) {
+    this.vistaActual = vista;
+  }
+}
